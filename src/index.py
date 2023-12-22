487b2283e745e9ffb72c402fb9613e45f3d473d9
idan054 <idanbit80@gmail.com>
2023-12-22 23:12:36 +0200

    vercel data based 11

--- src/index.py
+++ src/index.py
@@ -7,23 +7,15 @@ import os
 app = Flask(__name__)
 
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
 @app.route('/')
-def home():
-    with open(
-        os.path.join(os.getcwd(), '../data', 'data.json'), mode='r'
-    ) as my_file:
-        text = my_file.read()
-        return text
+def index():
+    return render_template('index.html')
 
 
 @app.route('/api')
 def api():
     with open(
-        os.path.join(os.getcwd(), 'data', 'data.json'), mode='r'
+        os.path.join(os.getcwd(), '../data', 'data.json'), mode='r'
     ) as my_file:
         text = my_file.read()
         return text
@@ -37,13 +29,7 @@ def success():
         data = product_details_by_name(main_field)
         addRow(data)
 
-    with open(
-        os.path.join(os.getcwd(), 'data', 'data.json'), mode='r'
-    ) as my_file:
-        text = my_file.read()
-
-    # return render_template('success.html')
-    return text
+    return render_template('success.html')
 
 # Debug only
 app.run(host='0.0.0.0', port=94, debug=True)
