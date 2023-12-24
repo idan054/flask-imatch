4f88a9496cf15cdaa53d97f07757874a2692ec8a
idan054 <idanbit80@gmail.com>
2023-12-24 23:47:40 +0200

    Web version (: V5

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask
 from handlers.routes import configure_routes
 import webview
 
+from modules.addRow import addRows
+
 app = Flask(__name__)
-window = webview.create_window('iMatch Tool', app, height=300, width=500)
+# window = webview.create_window('iMatch Tool', app, height=300, width=500)
 
 configure_routes(app)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=4012, host='0.0.0.0')
+    addRows(['X', 'Y', 'Z'])
+    # app.run(debug=True, port=4012, host='0.0.0.0')
     # webview.start(http_server=True, http_port=5000)
 
 # Build .exe file
--- modules/addRow.py
+++ modules/addRow.py
@@ -8,14 +8,16 @@ def addRows(row_values):
     print(row_values)
     
     credentials = ServiceAccountCredentials.from_json_keyfile_name('static/data/data.json')
-    client = gspread.authorize(credentials)    
+    client = gspread.authorize(credentials)
+    print(client.auth.token)
     spreadsheet_list = client.list_spreadsheet_files()
     print("spreadsheet_list")
     print(spreadsheet_list)
     # client.create(title="iMatch products")
-    
-    sh = client.open_by_key('1FfmbtfQaFDflDk1BBSWIJMULc0f-5L63LbvPFdhczBc')
-    # sh.share('idanbit80@gmail.com', perm_type='user', role='writer', with_link=True,)
+
+    sh = client.open_by_key('1QtOypISG4v4EScuH8hly_DpS27vPbA3-xvKGCy-PvSg')
+
+    sh.share('idanbit80@gmail.com', perm_type='user', role='writer', with_link=True,)
     worksheet = sh.get_worksheet(0)
 
     if isinstance(row_values[0], list):
