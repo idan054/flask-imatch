2f1f855f7f3a5f6131bb26b43d72ab59045acd36
idan054 <idanbit80@gmail.com>
2023-12-24 21:06:07 +0200

    vercel the Tamplate 17 - avner.

--- modules/woo_requests/get_categories.py
+++ modules/woo_requests/get_categories.py
@@ -13,16 +13,20 @@ def get_woo_categories():
     # consumer_secret = 'cs_5265e43f6e72275fc510c86dee08ae81b08c8e97'
     # product_name = 'חומר גלם'
 
-    file_path = 'settings.txt'
-    config = {}
-    with open(file_path, 'r') as file:
-        for line in file:
-            key, value = line.strip().split('=')
-            config[key.strip()] = value.strip()
+    # file_path = 'settings.txt'
+    # config = {}
+    # with open(file_path, 'r') as file:
+    #     for line in file:
+    #         key, value = line.strip().split('=')
+    #         config[key.strip()] = value.strip()
+    #
+    # base_url = config.get('base_url', '')
+    # consumer_key = config.get('consumer_key', '')
+    # consumer_secret = config.get('consumer_secret', '')
 
-    base_url = config.get('base_url', '')
-    consumer_key = config.get('consumer_key', '')
-    consumer_secret = config.get('consumer_secret', '')
+    base_url = 'https://avners.co.il'
+    consumer_key = 'ck_40277bba2e0d959fb2ec3aed808e01db800f4c1f'
+    consumer_secret = 'cs_ffefd520797cef75c3a9c5cea3dd477c2fa585bf'
 
     endpoint = f'{base_url}/wp-json/wc/v3/products/categories?per_page=100&_fields=id,name'
     response = requests.get(endpoint, auth=HTTPBasicAuth(consumer_key, consumer_secret))
