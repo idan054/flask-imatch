e18e9cc68fbdcb1cbe4ab8ecf4808510e6da91dc
idan054 <idanbit80@gmail.com>
2023-12-24 21:16:57 +0200

    vercel the Tamplate 20 - avner.

--- modules/woo_requests/get_categories.py
+++ modules/woo_requests/get_categories.py
@@ -8,9 +8,9 @@ from woocommerce import API
 
 
 def get_woo_categories():
-    # base_url = 'https://spider3d.co.il'
-    # consumer_key = 'ck_10860d370ddb79f39b4da3a765960cfd05842cfa'
-    # consumer_secret = 'cs_5265e43f6e72275fc510c86dee08ae81b08c8e97'
+    base_url = 'https://spider3d.co.il'
+    consumer_key = 'ck_10860d370ddb79f39b4da3a765960cfd05842cfa'
+    consumer_secret = 'cs_5265e43f6e72275fc510c86dee08ae81b08c8e97'
     # product_name = 'חומר גלם'
 
     # file_path = 'settings.txt'
@@ -24,21 +24,21 @@ def get_woo_categories():
     # consumer_key = config.get('consumer_key', '')
     # consumer_secret = config.get('consumer_secret', '')
 
-    base_url = 'https://avners.co.il'
-    consumer_key = 'ck_40277bba2e0d959fb2ec3aed808e01db800f4c1f'
-    consumer_secret = 'cs_ffefd520797cef75c3a9c5cea3dd477c2fa585bf'
+    # base_url = 'https://avners.co.il'
+    # consumer_key = 'ck_40277bba2e0d959fb2ec3aed808e01db800f4c1f'
+    # consumer_secret = 'cs_ffefd520797cef75c3a9c5cea3dd477c2fa585bf'
 
     endpoint = f'{base_url}/wp-json/wc/v3/products/categories?per_page=100&_fields=id,name'
     response = requests.get(endpoint, auth=HTTPBasicAuth(consumer_key, consumer_secret))
 
-    if response.status_code == 200:
-        categories = response.json()
+    # if response.status_code == 200:
+    categories = response.json()
 
         # file_path = os.path.join(os.getcwd(), 'categories.json')
         # Write the categories to a file
         # with open(file_path, mode='w') as file:
         #     json.dump(categories, file, ensure_ascii=False, indent=4)
 
-        return response.json()
+    return response.json()
 
-    return None
+    # return None
