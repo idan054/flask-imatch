428b9044a03da4b184bb2132bbcd2b9c297d2347
idan054 <idanbit80@gmail.com>
2023-12-24 23:03:20 +0200

    Web version (: V3

--- modules/woo_requests/get_categories.py
+++ modules/woo_requests/get_categories.py
@@ -25,38 +25,44 @@ def get_woo_categories(web_field, cs_field, ck_field, ):
     # consumer_key = config.get('consumer_key', '')
     # consumer_secret = config.get('consumer_secret', '')
 
-    endpoint = f'{base_url}/wp-json/wc/v3/products/categories?per_page=100&_fields=id,name'
-    response = requests.get(endpoint, auth=HTTPBasicAuth(consumer_key, consumer_secret))
-
-    if response.status_code == 200:
-        categories = response.json()
-
-        # file_path = os.path.join(os.getcwd(), 'categories.json')
-        # Write the categories to a file
-        # with open(file_path, mode='w') as file:
-        #     json.dump(categories, file, ensure_ascii=False, indent=4)
-
-        return response.json()
-
-    return [
+    errList = [
 
         {
             'id': 0,
-            'name': 'נא לפנות אל מנהל האתר / האיש הטכני',
+            'name': '1. קטגוריות לא זוהו - נא לפנות אל האיש הטכני / מנהל האתר',
         },
 
         {
             'id': 1,
-            'name': 'קטגוריות לא זוהו: 1. יש לוודא שהמפתח CS CK תקינים *והמפתח משוייך למשתמש מנהל!*',
+            'name': '2. קטגוריות לא זוהו - יש לוודא שהמפתח CS CK תקינים *והמפתח משוייך למשתמש מנהל!*',
         },
 
         {
             'id': 2,
-            'name': 'קטגוריות לא זוהו: 2. יש לוודא שכתובת האתר הוזנה בצורה תקינה https://your-site.co.il',
+            'name': '3. קטגוריות לא זוהו - יש לוודא שכתובת האתר הוזנה בצורה תקינה https://your-site.co.il',
         },
 
         {
             'id': 3,
-            'name': 'קטגוריות לא זוהו. ייתכן שהשרת / אתר שלך חוסם את הבקשה כאמצעי הגנה למניעת CORS',
+            'name': '4. קטגוריות לא זוהו - ייתכן שהשרת / אתר שלך חוסם את הבקשה כאמצעי הגנה למניעת CORS',
         }
     ]
+
+    try:
+        endpoint = f'{base_url}/wp-json/wc/v3/products/categories?per_page=100&_fields=id,name'
+        response = requests.get(endpoint, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+
+        if response.status_code == 200:
+            categories = response.json()
+
+            # file_path = os.path.join(os.getcwd(), 'categories.json')
+            # Write the categories to a file
+            # with open(file_path, mode='w') as file:
+            #     json.dump(categories, file, ensure_ascii=False, indent=4)
+
+            return response.json()
+
+        return errList
+
+    except:
+        return errList
