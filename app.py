a0a8fa7dd80f6ab583370427531c019847760983
idan054 <idanbit80@gmail.com>
2023-12-24 21:03:03 +0200

    vercel the Tamplate 16 - avner.

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ window = webview.create_window('iMatch Tool', app, height=300, width=500)
 configure_routes(app)
 
 if __name__ == "__main__":
-    # app.run(debug=True, port=4010, host='0.0.0.0')
-    webview.start(http_server=True, http_port=5000)
+    app.run(debug=True, port=4010, host='0.0.0.0')
+    # webview.start(http_server=True, http_port=5000)
 
 # Build .exe file
 # pyinstaller --noconfirm --onefile --windowed "/Users/mac/PycharmProjects/flask-imatch/app.py"
